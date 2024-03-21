5e1552335162a767239b7097c57dc33c0b933391
petersudai <psudai@gmail.com>
2024-03-20 18:57:06 -0700

    main delete

--- console.py
+++ console.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ Console Module """
 import sys
-sys.path.append('/AirBnB_clone')
 import cmd
 from models.base_model import BaseModel
-from models import storage
+from models.__init__ import storage
 from datetime import datetime
 from models.user import User
 from models.place import Place
