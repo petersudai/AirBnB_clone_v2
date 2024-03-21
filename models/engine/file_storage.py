8650308b61b73d10b13dd9a2dec9a786344fce1c
petersudai <psudai@gmail.com>
2024-03-20 18:55:08 -0700

    file storage update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from datetime import datetime
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.amenity import Amenity
-from models.city import City
-from models.review import Review
+import os
 
 
 class FileStorage:
@@ -35,6 +28,13 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.amenity import Amenity
+        from models.city import City
+        from models.review import Review
 
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
