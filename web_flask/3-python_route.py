b3326ba869a0ee83a854062fd0c36ac91810c523
petersudai <psudai@gmail.com>
2024-04-19 04:33:02 -0700

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,6 +25,7 @@ def c_is_fun(text):
     """Displays 'C <text>'"""
     return 'C {}'.format(text.replace('_', ' '))
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text="is cool"):
