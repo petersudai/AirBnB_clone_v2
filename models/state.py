9ef40a452f9b3384cf5b466bd129b1a60ddd0e21
petersudai <psudai@gmail.com>
2024-04-20 09:05:44 -0700

    Improve engines

--- models/state.py
+++ models/state.py
@@ -5,4 +5,15 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        @property
+        def cities(self):
+            """Getter method for cities"""
+            cities_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
